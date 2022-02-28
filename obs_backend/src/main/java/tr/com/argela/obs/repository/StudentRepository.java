7ede0b9b838564681f29d749511c657b002d7445
Sule Onal <suleonal8@gmail.com>
2022-02-28 17:53:08 +0300

    ogrenci derslerini getir

--- obs_backend/src/main/java/tr/com/argela/obs/repository/StudentRepository.java
+++ obs_backend/src/main/java/tr/com/argela/obs/repository/StudentRepository.java
@@ -1,12 +1,18 @@
 package tr.com.argela.obs.repository;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import tr.com.argela.obs.entity.Lecture;
 import tr.com.argela.obs.entity.Student;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
-public interface StudentRepository extends JpaRepository<Student,Long> {
-
+  
 }
-
