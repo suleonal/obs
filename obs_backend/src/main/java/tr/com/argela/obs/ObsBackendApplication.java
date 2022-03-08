a913aeeb3da3c3b9c432a3c0887265e911d01b92
Sule Onal <suleonal8@gmail.com>
2022-03-08 16:25:01 +0300

    swagger uygulaması

--- obs_backend/src/main/java/tr/com/argela/obs/ObsBackendApplication.java
+++ obs_backend/src/main/java/tr/com/argela/obs/ObsBackendApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class ObsBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
