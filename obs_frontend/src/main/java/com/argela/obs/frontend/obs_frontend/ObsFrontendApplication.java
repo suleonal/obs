ffde8fc2f39ec308fbae080641c1e42fc1883826
Sule Onal <suleonal8@gmail.com>
2022-03-09 16:19:43 +0300

    client app

---
+++ obs_frontend/src/main/java/com/argela/obs/frontend/obs_frontend/ObsFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.argela.obs.frontend.obs_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsFrontendApplication.class, args);
+	}
+
+}
---
+++ obs_frontend/src/test/java/com/argela/obs/frontend/obs_frontend/ObsFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argela.obs.frontend.obs_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
